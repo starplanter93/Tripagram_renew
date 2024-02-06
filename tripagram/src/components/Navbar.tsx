bec160e0ab1be7b4a566a5a8449483d84fba651d
Kyusik-Jeong <starplanter93@gmail.com>
2024-02-06 17:18:41 +0900

    Navbar 및 아이콘 추가

---
+++ tripagram/src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+'use client';
+import Link from 'next/link';
+import HomeIcon from './ui/icons/HomeIcon';
+import HomeFillIcon from './ui/icons/HomeFillIcon';
+import SearchIcon from './ui/icons/SearchIcon';
+import SearchFillIcon from './ui/icons/SearchFillIcon';
+import NewIcon from './ui/icons/NewIcon';
+import NewFillIcon from './ui/icons/NewFillIcon';
+import { usePathname } from 'next/navigation';
+
+const menu = [
+  {
+    href: '/',
+    icon: <HomeIcon />,
+    clickedIcon: <HomeFillIcon />
+  },
+  {
+    href: '/search',
+    icon: <SearchIcon />,
+    clickedIcon: <SearchFillIcon />
+  },
+  {
+    href: '/new',
+    icon: <NewIcon />,
+    clickedIcon: <NewFillIcon />
+  }
+];
+export default function Navbar() {
+  const pathName = usePathname();
+  return (
+    <div>
+      <Link href='/'>Tripagram</Link>
+      <nav>
+        <ul>
+          {menu.map((item) => (
+            <li key={item.href}>
+              <Link href={item.href}>
+                {pathName === item.href ? item.clickedIcon : item.icon}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
