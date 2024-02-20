ba2ab12321d82874f58d2de3be81b5b13534f184
starplanter93 <starplanter93@gmail.com>
2024-02-20 22:23:14 +0900

    팔로잉바 스타일링

--- tripagram/src/components/FollowingBar.tsx
+++ tripagram/src/components/FollowingBar.tsx
@@ -11,18 +11,24 @@ export default function FollowingBar() {
 
   console.log(data?.following);
   return (
-    <section>
+    <section className='w-full flex justify-center items-center p-4 shadow-sm shadow-neutral-300 mb-4 rounded-lg min-h-[90px] overflow-x-auto'>
       {loading ? (
         <PropagateLoader size={8} color='red' />
       ) : (
         (!users || users.length === 0) && <p>{`팔로잉한 유저 없음`}</p>
       )}
       {users && users.length > 0 && (
-        <ul>
+        <ul className='w-full flex gap-2'>
           {users.map(({ image, username }) => (
             <li key={username}>
-              <Link href={`/users/${username}`}>
+              <Link
+                className='flex flex-col items-center w-20'
+                href={`/users/${username}`}
+              >
                 <Avatar image={image} highlight />
+                <p className='w-full text-sm text-center text-ellipsis overflow-hidden'>
+                  {username}
+                </p>
               </Link>
             </li>
           ))}
