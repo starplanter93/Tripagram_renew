353131cb61017b00741820d977fec9375d71d80f
starplanter93 <starplanter93@gmail.com>
2024-02-24 22:21:34 +0900

    sanity 이미지 최적화

--- tripagram/src/components/PostList.tsx
+++ tripagram/src/components/PostList.tsx
@@ -6,6 +6,8 @@ export default function PostList() {
   const { data: posts, isLoading: loading } =
     useSWR<SimplePost[]>('/api/posts');
 
+  console.log(posts);
+
   return (
     <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
   );
--- tripagram/src/service/sanity.ts
+++ tripagram/src/service/sanity.ts
@@ -1,4 +1,6 @@
-import {createClient} from '@sanity/client'
+import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
@@ -6,4 +8,10 @@ export const client = createClient({
   useCdn: false, // set to `false` to bypass the edge cache
   apiVersion: '2024-02-13', // use current date (YYYY-MM-DD) to target the latest API version
   token: process.env.SANITY_SECRET_TOKEN, // Only if you want to update content with the client
-})
+});
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source: SanityImageSource) {
+  return builder.image(source).width(800).url();
+}
